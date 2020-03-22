22d886ab0c77301e7297a0e587f05b9c5cee0693
olgaklimova <olguna.klimova@mail.ru>
2020-03-23 00:32:06 +0300

    create index

--- main.go
+++ main.go
@@ -1,166 +1,129 @@
-//Открытие файла с инвертированным индексом осуществляется через os.Args[1]
-//Слова для поиска сичтываются с консоли
-
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
+	//Нахождение папки с файлами
 
-	file_with_ind := os.Args[1]
-	// var file_with_ind string
-	// file_with_ind = "C:\\Users\\asus\\source\\repos\\hello\\invertindex.txt"
+	papka := os.Args[1]
 
-	var allword, sentence, indstr, simind string
-	var k, y, m, elemcount, elem, wordlatters, length, indlatters int
-	search_words := make(map[int]string)
-	indtext := make(map[int][]byte)
-	senttext := make([]byte, 1000)
-	word := make([]byte, 100)
-	filetext := make([]byte, 10000000)
-	indword := make([]byte, 100)
-	indexes := make([]byte, 100)
-	index := make([]byte, 100)
-	allindexes := make(map[string]string)
-
-	openfile, err := os.Open(file_with_ind)
+	dir, err := os.Open(papka)
 	if err != nil {
 		return
 	}
-	defer openfile.Close()
+	defer dir.Close()
 
-	fmt.Print("\nВведите сочетание слов для поиска: ")
-
-	in := bufio.NewReader(os.Stdin)
-
-	sentence, err = in.ReadString('\n')
+	fileInfos, err := dir.Readdir(-1)
 	if err != nil {
-		fmt.Println("Ошибка ввода: ", err)
+		return
 	}
 
-	senttext = []byte(sentence)
-	length = len(senttext)
-	for elemcount < length {
-		for count := 0; elemcount < len(senttext) && senttext[elemcount] != ' ' && senttext[elemcount] != '"' && senttext[elemcount] != '\n' && senttext[elemcount] != ':' && senttext[elemcount] != '.' && senttext[elemcount] != 39 && senttext[elemcount] != 45 && senttext[elemcount] != 150 && senttext[elemcount] != 151; elemcount++ {
-			if (senttext[elemcount] < 65) || senttext[elemcount] > 122 || (senttext[elemcount] < 97 && senttext[elemcount] > 90) {
-			} else {
-				if senttext[elemcount] > 64 && senttext[elemcount] < 91 {
-					senttext[elemcount] = senttext[elemcount] + 32
-				}
-				word[count] = senttext[elemcount]
-				count++
-				wordlatters++
-			}
-		}
-		elemcount++
+	fmt.Println("\nВ папке были найдены файлы:")
+	var nfile, records, newrecords, wordlatters, check, elemcount int
+	var allword, indfi string
+	index := make(map[int]string)
+	indexfile := make(map[int]string)
+	numberfile := make(map[int]int)
 
-		//Запись каждого слова, считанного с консоли
-		for j := 0; j < wordlatters; j++ {
-			allword = (allword + string(word[j]))
-			word[j] = 0
-		}
+	for _, fi := range fileInfos {
 
-		search_words[k] = allword
-		k++
-		allword = ""
-		wordlatters = 0
-	}
+		fmt.Println(fi.Name())
+		nfile++
+		openfile, err := os.Open(papka + "/" + fi.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer openfile.Close()
 
-	for {
-		_, err := openfile.Read(filetext) //Считываем всю информацию из файла
-		if err == io.EOF {
-			break
-		}
-	}
-
-	for filetext[elem] != 0 {
-		for count := 0; filetext[elem] != ' ' && filetext[elem] != 0 && filetext[elem] != '\n'; elem++ {
-			if filetext[elem] < 65 || filetext[elem] > 122 || (filetext[elem] < 97 && filetext[elem] > 90) {
-			} else {
-				indword[count] = filetext[elem]
-				count++
-				wordlatters++
-			}
-		}
-		elem++
-
-		//Запись каждого слова, считанного из файла
-		for j := 0; j < wordlatters; j++ {
-			simind = (simind + string(indword[j]))
-			indword[j] = 0
-		}
-
-		for n := 0; n < k; n++ {
-			if simind == search_words[n] {
-				elem++
-				for count := 0; filetext[elem] != '}' && filetext[elem] != 0; elem++ {
-					index[count] = filetext[elem]
-					count++
-					indlatters++
-				}
-				elem++
-				for j := 0; j < indlatters; j++ {
-					indstr = (indstr + string(index[j]))
-					//Запись всех индексов текстов в массив байт
-					indexes[y] = index[j]
-					y++
-					index[j] = 0
-				}
-				//Создание map allindexes для хранения слова и номеров текстов, где оно встречается
-				allindexes[simind] = indstr
-				//Создание map allindexes для хранения слова и номеров(в байтовов представлении) текстов, где оно встречается
-				indtext[m] = []byte(indstr)
-				m++
-			}
-		}
-		indstr = ""
-		simind = ""
-		wordlatters = 0
-		indlatters = 0
-	}
-
-	var n, w int
-	var firstnumber byte
-	firstnumber = indexes[0]
-	indexes[0] = 0
-	n = 1
-	//Цикл for, осуществляющий нахождение совпадений в массиве indexes[]
-	for w < y {
-		for count := 0; count < y; count++ {
-			if indexes[count] == 32 || indexes[count] == 0 {
-				indexes[count] = 0
-			} else {
-				if indexes[count] == firstnumber {
-					n++
-					indexes[count] = 0
-				}
-			}
-		}
-		if n != m {
-		} else {
-			indstr = (indstr + " " + string(firstnumber))
-		}
-		n = 0
-		for count := 0; count < y; count++ {
-			if indexes[count] == 32 || indexes[count] == 0 {
-			} else {
-				firstnumber = indexes[count]
-				indexes[count] = 0
-				n = 1
+		filetext := make([]byte, 100000000)
+		word := make([]byte, 100)
+		for {
+			_, err := openfile.Read(filetext) //Считываем всю информацию из файла
+			if err == io.EOF {
 				break
 			}
 		}
-		w++
+
+		//Проверка считанного текста по элементам
+		for filetext[elemcount] != 0 {
+			for count := 0; filetext[elemcount] != ' ' && filetext[elemcount] != 0 && filetext[elemcount] != '"' && filetext[elemcount] != '\n' && filetext[elemcount] != ':' && filetext[elemcount] != '.' && filetext[elemcount] != 39 && filetext[elemcount] != 45 && filetext[elemcount] != 150 && filetext[elemcount] != 151; elemcount++ {
+				if (filetext[elemcount] < 65) || filetext[elemcount] > 122 || (filetext[elemcount] < 97 && filetext[elemcount] > 90) {
+				} else {
+					if filetext[elemcount] > 64 && filetext[elemcount] < 91 {
+						filetext[elemcount] = filetext[elemcount] + 32
+					}
+					word[count] = filetext[elemcount]
+					count++
+					wordlatters++
+				}
+			}
+			elemcount++
+
+			//Запись каждого слова
+			for j := 0; j < wordlatters; j++ {
+
+				allword = (allword + string(word[j]))
+				word[j] = 0
+			}
+
+			//Запись слова в инвертированный индекс
+			for k := 0; k <= records; k++ {
+				if allword == index[k] {
+					if nfile == 1 {
+						numberfile[k] = nfile
+						indfi = indexfile[records]
+					} else {
+						if k < records-newrecords {
+							indfi = indexfile[k]
+							nf := strconv.Itoa(nfile)
+							if numberfile[k] != nfile {
+								indexfile[k] = (indfi + nf + " ")
+							}
+							numberfile[k] = nfile
+						}
+					}
+					check = 1
+				}
+			}
+
+			if check == 0 {
+				for k := 0; k <= records; k++ {
+					index[records] = allword
+					check = 0
+					nf := strconv.Itoa(nfile)
+					indexfile[records] = (" " + nf + " ")
+				}
+				newrecords++
+				records++
+			}
+			wordlatters = 0
+			allword = ""
+			check = 0
+		}
+		newrecords = 0
+		elemcount = 0
 	}
 
-	if indstr != "" {
-		fmt.Println("Введенные слова были найдены в текстах:" + indstr + "\n")
-	} else {
-		fmt.Println("Введенные слова не встречаются вместе")
+	//Создание файла и запись полученного индекса
+
+	newfile, err := os.Create("invertindex.txt")
+	if err != nil {
+		fmt.Println("Unable to create file:", err)
+		os.Exit(1)
 	}
+	for k := 0; k < records; k++ {
+		newfile.WriteString(index[k])
+		newfile.WriteString(" {")
+		newfile.WriteString(indexfile[k])
+		newfile.WriteString("};\n")
+	}
+
+	defer newfile.Close()
+	fmt.Println("\nФайл с инвертированным индексом записан")
 }
