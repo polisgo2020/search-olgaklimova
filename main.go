82870d11321ab24e54944aa0aaa4d83c9223f5a9
olgaklimova <olguna.klimova@mail.ru>
2020-03-31 12:48:16 +0300

    parallel indexing

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+//os.Args[1] - путь к папке
+//os.Args[2] - путь к файлу для записи индекса
 package main
 
 import (
@@ -5,14 +7,122 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
+	"sync"
+	"time"
 )
+
+var index = make(map[int]string)
+var indexfile = make(map[int]string)
+var numberfile = make(map[int]int)
+var mu = &sync.Mutex{}
+var records int
+var newrecords int
+
+func openfile(fname chan string, nfile int) {
+	var alltext chan []byte = make(chan []byte)
+	filetext := make([]byte, 100000000)
+LOOP:
+	for {
+		select {
+		case str := <-fname:
+			openfile, err := os.Open(str)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer openfile.Close()
+		LOOP1:
+			for {
+				_, err := openfile.Read(filetext) //Считываем всю информацию из файла
+				if err == io.EOF {
+					break LOOP1
+				}
+			}
+			go textAnalysis(alltext, nfile)
+			alltext <- filetext
+		default:
+			break LOOP
+		}
+		close(alltext)
+		break LOOP
+	}
+}
+
+func textAnalysis(alltext chan []byte, n int) {
+	var elemcount, wordlatters int
+	var allword string
+	word := make([]byte, 30)
+LOOP2:
+	for {
+		select {
+		case text := <-alltext:
+			for text[elemcount] != 0 {
+				for count := 0; text[elemcount] != ' ' && text[elemcount] != 0 && text[elemcount] != '"' && text[elemcount] != '\n' && text[elemcount] != ':' && text[elemcount] != '.' && text[elemcount] != 39 && text[elemcount] != 45 && text[elemcount] != 150 && text[elemcount] != 151; elemcount++ {
+					if (text[elemcount] < 65) || text[elemcount] > 122 || (text[elemcount] < 97 && text[elemcount] > 90) {
+					} else {
+						if text[elemcount] > 64 && text[elemcount] < 91 {
+							text[elemcount] = text[elemcount] + 32
+						}
+						word[count] = text[elemcount]
+						count++
+						wordlatters++
+					}
+				}
+				elemcount++
+
+				for j := 0; j < wordlatters; j++ {
+					allword = (allword + string(word[j]))
+					word[j] = 0
+				}
+				go createIndex(n, allword)
+				wordlatters = 0
+				allword = ""
+			}
+			elemcount = 0
+		default:
+			break LOOP2
+		}
+		break LOOP2
+	}
+}
+
+func createIndex(n int, allword string) {
+	mu.Lock()
+	var check int
+	var indfi string
+	for k := 0; k <= records; k++ {
+		if allword == index[k] {
+			if k < records {
+				indfi = indexfile[k]
+				nf := strconv.Itoa(n)
+				if strings.Contains(indexfile[k], nf) != true {
+					indexfile[k] = (indfi + nf + " ")
+				}
+			}
+			check = 1
+		}
+	}
+	if check == 0 {
+		index[records] = allword
+		nf := strconv.Itoa(n)
+		indexfile[records] = (" " + nf + " ")
+		check = 0
+		records++
+	}
+	check = 0
+	mu.Unlock()
+}
 
 func main() {
 	//Нахождение папки с файлами
-
 	papka := os.Args[1]
+	// var papka string
+	// papka = ("C:\\Users\\asus\\source\\repos\\hello\\files")
 
+	fmt.Println("\nВ папке были найдены файлы:")
+	var nfile int
 	dir, err := os.Open(papka)
 	if err != nil {
 		return
@@ -24,106 +134,31 @@ func main() {
 		return
 	}
 
-	fmt.Println("\nВ папке были найдены файлы:")
-	var nfile, records, newrecords, wordlatters, check, elemcount int
-	var allword, indfi string
-	index := make(map[int]string)
-	indexfile := make(map[int]string)
-	numberfile := make(map[int]int)
-
+	var fname chan string = make(chan string)
 	for _, fi := range fileInfos {
-
 		fmt.Println(fi.Name())
 		nfile++
-		openfile, err := os.Open(papka + "/" + fi.Name())
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer openfile.Close()
-
-		filetext := make([]byte, 100000000)
-		word := make([]byte, 100)
-		for {
-			_, err := openfile.Read(filetext) //Считываем всю информацию из файла
-			if err == io.EOF {
-				break
-			}
-		}
-
-		//Проверка считанного текста по элементам
-		for filetext[elemcount] != 0 {
-			for count := 0; filetext[elemcount] != ' ' && filetext[elemcount] != 0 && filetext[elemcount] != '"' && filetext[elemcount] != '\n' && filetext[elemcount] != ':' && filetext[elemcount] != '.' && filetext[elemcount] != 39 && filetext[elemcount] != 45 && filetext[elemcount] != 150 && filetext[elemcount] != 151; elemcount++ {
-				if (filetext[elemcount] < 65) || filetext[elemcount] > 122 || (filetext[elemcount] < 97 && filetext[elemcount] > 90) {
-				} else {
-					if filetext[elemcount] > 64 && filetext[elemcount] < 91 {
-						filetext[elemcount] = filetext[elemcount] + 32
-					}
-					word[count] = filetext[elemcount]
-					count++
-					wordlatters++
-				}
-			}
-			elemcount++
-
-			//Запись каждого слова
-			for j := 0; j < wordlatters; j++ {
-
-				allword = (allword + string(word[j]))
-				word[j] = 0
-			}
-
-			//Запись слова в инвертированный индекс
-			for k := 0; k <= records; k++ {
-				if allword == index[k] {
-					if nfile == 1 {
-						numberfile[k] = nfile
-						indfi = indexfile[records]
-					} else {
-						if k < records-newrecords {
-							indfi = indexfile[k]
-							nf := strconv.Itoa(nfile)
-							if numberfile[k] != nfile {
-								indexfile[k] = (indfi + nf + " ")
-							}
-							numberfile[k] = nfile
-						}
-					}
-					check = 1
-				}
-			}
-
-			if check == 0 {
-				for k := 0; k <= records; k++ {
-					index[records] = allword
-					check = 0
-					nf := strconv.Itoa(nfile)
-					indexfile[records] = (" " + nf + " ")
-				}
-				newrecords++
-				records++
-			}
-			wordlatters = 0
-			allword = ""
-			check = 0
-		}
-		newrecords = 0
-		elemcount = 0
+		var str string = (papka + "/" + fi.Name())
+		go openfile(fname, nfile)
+		fname <- str
 	}
+	close(fname)
+
+	time.Sleep(time.Millisecond * 100)
 
 	//Создание файла и запись полученного индекса
-
 	newfile, err := os.Create("invertindex.txt")
 	if err != nil {
 		fmt.Println("Unable to create file:", err)
 		os.Exit(1)
 	}
-	for k := 0; k < records; k++ {
-		newfile.WriteString(index[k])
-		newfile.WriteString(" {")
-		newfile.WriteString(indexfile[k])
-		newfile.WriteString("};\n")
-	}
 
+	for k := 0; k < records; k++ {
+		mu.Lock()
+		newfile.WriteString(filepath.Join(index[k] + " {" + indexfile[k] + "}\n"))
+		mu.Unlock()
+	}
 	defer newfile.Close()
 	fmt.Println("\nФайл с инвертированным индексом записан")
+	indexSearch()
 }
